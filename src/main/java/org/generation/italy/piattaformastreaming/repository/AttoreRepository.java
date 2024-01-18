d4ec9d26a25c8d34b7a438c207ca72668f29b040
Fanasca Fabrizio <fabri.taz5@live.it>
2024-01-18 12:46:57 +0100

    aggiunto l'annotation repository

--- src/main/java/org/generation/italy/piattaformastreaming/repository/AttoreRepository.java
+++ src/main/java/org/generation/italy/piattaformastreaming/repository/AttoreRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.generation.italy.piattaformastreaming.model.Attore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AttoreRepository extends JpaRepository<Attore, Integer>{
 
 	List<Attore> findByNomeLike (String nome);
