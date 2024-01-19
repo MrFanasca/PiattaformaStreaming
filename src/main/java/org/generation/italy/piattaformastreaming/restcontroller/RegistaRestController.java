77dc32f100ee86dec13e4956830ad3fd6625a5c7
Fanasca Fabrizio <fabri.taz5@live.it>
2024-01-19 10:38:33 +0100

    sostituito @RestController

--- src/main/java/org/generation/italy/piattaformastreaming/restcontroller/RegistaRestController.java
+++ src/main/java/org/generation/italy/piattaformastreaming/restcontroller/RegistaRestController.java
@@ -8,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/api/regista")
+@RestController
+@RequestMapping("/api/Regista")
 public class RegistaRestController {
 	
 	@Autowired
